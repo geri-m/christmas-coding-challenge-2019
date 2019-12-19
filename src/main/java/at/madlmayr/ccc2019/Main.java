1cbb913372c1fdca56b94a60016f4567b65769ce
Gerald Madlmayr <gerald.madlmayr@gmx.at>
2019-12-19 20:01:45 +0100

    Adding Frist test in red

--- src/main/java/at/madlmayr/ccc2019/Main.java
+++ src/main/java/at/madlmayr/ccc2019/Main.java
@@ -1,4 +1,21 @@
 package at.madlmayr.ccc2019;
 
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+
 public class Main {
+
+    public boolean regex(String pattern, String s) {
+        return false;
+    }
+
+    @Test
+    public void singleChar() {
+        assertThat(regex("a", "a"), equalTo(true));
+    }
+
+
 }
