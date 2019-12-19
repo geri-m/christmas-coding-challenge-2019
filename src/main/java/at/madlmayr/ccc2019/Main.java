3bae6e13345a9b74f24457c24d6371d00619c73a
Gerald Madlmayr <gerald.madlmayr@gmx.at>
2019-12-19 20:12:49 +0100

    2nd test green

--- src/main/java/at/madlmayr/ccc2019/Main.java
+++ src/main/java/at/madlmayr/ccc2019/Main.java
@@ -9,12 +9,40 @@ import static org.hamcrest.core.IsEqual.equalTo;
 public class Main {
 
     public boolean regex(String pattern, String s) {
-        return s.contains(pattern);
+
+        for (int indexOfPattern = 0; indexOfPattern < pattern.length(); indexOfPattern++){
+            char charOfPattern = pattern.charAt(indexOfPattern);
+
+            if(charOfPattern == '*') {
+
+            } else {
+                int indexOfCharOfPattern = s.indexOf(charOfPattern);
+                if(indexOfCharOfPattern < 0)
+                    return false;
+            }
+        }
+
+        return true;
     }
 
     @Test
     public void singleChar() {
         assertThat(regex("a", "a"), equalTo(true));
+        assertThat(regex("a", "aa"), equalTo(true));
+        assertThat(regex("a", "ab"), equalTo(true));
+        assertThat(regex("a", "ba"), equalTo(true));
+        assertThat(regex("a", "b"), equalTo(false));
+        assertThat(regex("a", "bb"), equalTo(false));
+    }
+
+    @Test
+    public void singleStar(){
+        assertThat(regex("*", "a"), equalTo(true));
+        assertThat(regex("*", "aa"), equalTo(true));
+        assertThat(regex("*", "ab"), equalTo(true));
+        assertThat(regex("*", "ba"), equalTo(true));
+        assertThat(regex("*", "b"), equalTo(true));
+        assertThat(regex("*", "bb"), equalTo(true));
     }
 
 
